7a5a8bbbb522f136302993aa9d2d9852b547f89d
munjia <munjia00@gmail.com>
2025-10-23 19:27:41 +0800

    Update df_gls.R

--- data_processing/df_gls.R
+++ data_processing/df_gls.R
@@ -14,42 +14,53 @@ library(urca)
 library(ggplot2)
 
 #read in data
-file_path = "../data/cleaned_goyal_predictors.xlsx"
+file_path = "../data/raw_predictors.xlsx"
 df = read_excel(file_path)
-df = df %>% select (yyyymm, ret, Rfree, price, d12, e12, lty, tms, dfy, dfr, svar, infl, tbl, ltr, corpr,
-                    lzrt, ogap, dtoy, dtoat, avgcor, fbm, skvw, tail, rdsp, ntis, erp, BAA, AAA)
-
 df_diff = df %>% mutate(date = as.Date(paste0(sprintf("%06d", yyyymm), "01"), "%Y%m%d"))%>% select(-yyyymm) %>%
-  select(date, erp, everything())
+  mutate(erp = ret - Rfree) %>% select(date, erp, everything())
 
-#determined by looking at the plots of the indicators
 model_dict <- list(
   ret = "constant",
   Rfree = "trend",
-  price = "trend",
   d12 = "trend",
   e12 = "trend",
+  BAA = "trend",
   lty = "trend",
+  d_p = "constant",
+  d_y = "constant",
+  e_p = "constant",
+  d_e = "constant",
   tms = "constant",
   dfy = "constant",
   dfr = "constant",
   svar = "constant",
   infl = "constant",
-  tbl = "constant",
+  AAA = "trend",
+  tbl = "trend",
+  b_m = "constant",
   ltr = "constant",
   corpr = "constant", 
   lzrt = "constant",
-  ogap = "constant",
+  wtexas = "constant",
   dtoy = "constant",
   dtoat = "constant",
-  avgcor = "trend",
-  fbm = "trend",
+  avgcor = "constant",
   skvw = "constant",
-  tail = "trend",
+  tail = "constant",
   rdsp = "constant",
   ntis = "trend",
-  BAA = "trend",
-  AAA = "trend"
+  ygap = "constant",
+  ndrbl = "constant",
+  rsvix  = "constant",
+  vrp = "constant",
+  impvar = "constant",
+  Volume = "trend",
+  MA_spread_1_9 = "constant",
+  MA_spread_1_12 = "constant",
+  MA_spread_2_9 = "constant",
+  MA_spread_2_12 = "constant",
+  MA_spread_3_9 = "constant",
+  MA_spread_3_12 = "constant"
   
 )
 
@@ -110,50 +121,20 @@ make_stationary_dfgls <- function(df, model_dict, max_lags = 4, max_differences 
 df_stationary = make_stationary_dfgls(df_diff, model_dict = model_dict) %>% drop_na()
 
 #helper to create lags
-lag_features <- function(data, lags = 4) {
+lag_features <- function(data, lags = 3) {
   data %>%
     mutate(across(
       .cols = -date,  # Exclude the date column
       .fns = list(
         lag1 = ~ lag(., 1),
         lag2 = ~ lag(., 2),
-        lag3 = ~ lag(., 3),
-        lag4 = ~ lag(., 4)
+        lag3 = ~ lag(., 3)
       ),
-      .names = "{.col}_{.fn}"  # Naming format: "GDP_lag1", "CPI_lag2", etc.
+      .names = "{.col}_{.fn}" 
     ))
 }
-df_lagged = lag_features(df_stationary) %>% arrange(desc(date)) %>% drop_na()
-df_lagged_std = df_lagged %>% mutate(across(where(is.numeric) & !any_of(c("date","erp")),
-                                                ~ (.-mean(., na.rm=TRUE))/sd(., na.rm=TRUE)))
-
-write.csv(df_lagged, "stationary_indicators.csv", row.names = FALSE)
-
-
-
-## plot graph of indicators to see if trend or constant for model_dict
-plot_stationarity <- function(df, date_col = "date", cols = NULL, add_trend = TRUE) {
-  stopifnot(date_col %in% names(df))
-  
-  if (is.null(cols)) {
-    cols <- names(df)[sapply(df, is.numeric)]
-  }
-  stopifnot(all(cols %in% names(df)))
-  
-  long <- df %>%
-    select(all_of(c(date_col, cols))) %>%
-    rename(date = !!date_col) %>%
-    pivot_longer(-date, names_to = "series", values_to = "value") %>%
-    arrange(date)
-  
-  p <- ggplot(long, aes(x = date, y = value)) +
-    geom_line() +
-    facet_wrap(~ series, scales = "free_y", ncol = 2) +
-    labs(x = NULL, y = NULL, title = "Visual stationarity check") +
-    theme_minimal(base_size = 12)
-  
-  if (add_trend) p <- p + geom_smooth(method = "lm", se = FALSE)
-  
-  print(p)
-}
-plot_stationarity(df_diff, date_col = "date", cols = c('AAA'))  
+df_lagged = lag_features(df_stationary) %>% arrange(desc(date)) %>% drop_na() 
+df_train= df_lagged %>% filter(row_number() > 100)
+df_test = df_lagged %>% filter(row_number() <= 100)
+write.csv(df_train, "stationary_indicators.csv", row.names = FALSE)
+write.csv(df_test, "df_test.csv", row.names = FALSE)
