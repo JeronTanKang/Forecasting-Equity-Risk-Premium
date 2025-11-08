a2a9d0a5e0e0eb5823882fca0aac7c764a175ca5
kellianneng <168501224+kellianneng@users.noreply.github.com>
2025-11-08 14:50:35 +0800

    Update prevailing_mean_benchmark.R

--- models/prevailing_mean_benchmark.R
+++ models/prevailing_mean_benchmark.R
@@ -1,9 +1,7 @@
 #Load Packages 
 library(readr)
 library(dplyr)
-library(pls)
-library(ggplot2)
-library(lubridate)
+library(writexl)
 
 file_path = "../data/stationary_indicators.csv"
 df = read.csv(file_path)
@@ -12,7 +10,28 @@ df = read.csv(file_path)
 df <- df %>% 
   arrange(date)
 
+# 2. Calculate Prevailing Mean 
 mu_hat = mean(df$erp,na.rm = TRUE)
 print(mu_hat) 
-
 #0.005817197
+
+
+####################################
+# Residuals of benchmark model for 
+# DM Test 
+####################################
+file_path_1 = "../data/full_df.csv"
+df1 = read.csv(file_path_1) %>% 
+  mutate(date = as.Date(date, format = "%m/%d/%y")) 
+
+prevailing_mean = 0.005817197   # Prevailing mean is hardcoded 
+cutoff <- as.Date("2015-05-01") # Test set 
+
+# Only have date + residual_col_name 
+df2 = df1 %>%
+  mutate(residual_col_name = erp - prevailing_mean) %>%
+  filter(date >= cutoff) %>% 
+  select(date, residual_col_name)
+
+# Save Residuals into excel for dm test 
+write_xlsx(df2, "../data/prev_mean_benchmark_residuals.xlsx")
